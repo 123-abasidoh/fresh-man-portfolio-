8ddfb5d3e13f7a33d6dadc0991080d15e85ca4ce
kennisjohn abasidoh <kennisjohnabasidoh1@gmail.com>
2024-05-29 12:51:53 +0100

    html css script portfolio fresh man level

---
+++ script.js
@@ -0,0 +1,37 @@
+let tabLinks = document.getElementsByClassName("tab-links");
+let tabcontents = document.getElementsByClassName("tab-contents");
+let sidemenu = document.getElementById("sidemenu");
+
+function opentab(tabname) {
+  for (tabLink of tabLinks) {
+    tabLink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+// email linking
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbxr8diebzb2p-0F1kIfZAt8Q66_CaeB9yaUg-s1B6FVlIvnjdhCQYCXPn3Qz0KffUU/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById('msg')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent sucessfully"
+        setTimeout(function () {
+          msg.innerHTML = ""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
